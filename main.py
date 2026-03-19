6db970bdbb4e5c09fee5f944c1b9cff8485be265
HJFS1 <jfarquharsmith@gmail.com>
2026-03-19 12:17:53 +0000

    Update main.py
    Implemented page config and imports

--- main.py
+++ main.py
@@ -1,1 +1,12 @@
+import streamlit as st
+import pandas as pd
+import plotly.express as px
+import plotly.graph_objects as go
+
+# ─────────────────────────── PAGE CONFIG ───────────────────────────
+st.set_page_config(
+    page_title="East London Housing Prices Dashboard",
+    page_icon="📊",
+    layout="wide",
+)
 
